a3169995dd432f81bf02eb889eef25831d70cac9
avmoran <avmoran@wustl.edu>
2018-01-17 12:53:14 -0600

    Connected app to database

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, session
+from flask import render_template, request, redirect, session, flash
 from app import app, db
 from models import User
 
@@ -9,14 +9,34 @@ def index():
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        if False:
-            ##TODO Verify information
+        username = request.form['username']
+        password = request.form['password']
+        verify_password = request.form['verify-password']
+        num_of_errors = 0
+
+        if len(username) < 3 or len(username) > 20:
+            num_of_errors += 1
+            flash('Username not valid.', 'error')
+        if len(password) < 3 or len(password) > 20:
+            num_of_errors += 1
+            flash('Password not valid.', 'error')
+        if password != verify_password:
+            num_of_errors += 1
+            flash('Passwords do not match.', 'error')
+        
+
+        if num_of_errors > 0:
             return redirect('/signup')
         else:
-            ##This is supposing sign-up is successful
+            session['username'] = username
+            user = User(username, password)
+            db.session.add(user)
+            db.session.commit()
             return "Sign-Up Successful"
 
     return render_template('signup.html', title = 'Sign-Up')
+
+app.secret_key = 'geJCtgNQcdPHJq9D'
 
 if __name__ == '__main__':
     app.run()
